4e86c93f11d10612418d7f37b64f343b83b423a2
Jhayren <emjhay345@gmail.com>
2024-12-11 11:10:12 +0800

    updates

--- connect.py
+++ connect.py
@@ -2,6 +2,21 @@ import mysql.connector
 import os
 from urllib.parse import urlparse
 
+'''
+def get_database_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="repository"
+        )
+        return connection
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+'''
+
 # Helper function to get a database connection
 def get_database_connection():
     try:
